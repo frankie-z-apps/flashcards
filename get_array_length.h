f55f0f48bbba063aa0af3715a5e25d5bde410fe9
Francesco Zappalà <zappala.francesco00@gmail.com>
2024-06-06 16:20:12 +0200

    add headers of auxiliary functions

---
+++ get_array_length.h
@@ -0,0 +1,6 @@
+#ifndef GET_ARRAY_LENGTH_H
+#define GET_ARRAY_LENGTH_H
+
+int get_array_length(char **arr);
+
+#endif
---
+++ is_whitespace.h
@@ -0,0 +1,6 @@
+#ifndef IS_WHITESPACE_H
+#define IS_WHITESPACE_H
+
+int is_whitespace(char c);
+
+#endif
