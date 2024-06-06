26f3ad04a987562288fdc4e6f19054a09c6c1284
Francesco Zappalà <zappala.francesco00@gmail.com>
2024-06-06 15:07:22 +0200

    implement filter function

--- filter_lines.c
+++ filter_lines.c
@@ -2,9 +2,9 @@
 #include <string.h>
 
 #define MAX_LINE_LENGTH 1024
-//#define INITIAL_ARRAY_SIZE 5
 
-int get_array_size(char **arr);
+int get_array_length(char **arr);
+int is_whitechar(char c);
 
 /*
 given an array of strings, exclude the strings beginning with a letter
@@ -16,24 +16,37 @@ char **filter_lines(char **rawlines)
 {
     char **filtered_lines = NULL;
     char buffer[MAX_LINE_LENGTH];
-    int array_size = get_array_size(rawlines);
+    int array_size = get_array_length(rawlines); // at most, length of raw array
+    int line = 0;
 
-}
-
-int get_array_size(char **arr) 
-{
-    if (!arr) {
-        fprintf(stderr, "Not a valid array.\n");
+    filtered_lines = malloc(array_size * sizeof(char *));
+    if (!filtered_lines) {
+        fprintf(stderr, "Error while allocating memory.\n");
         return 1;
     }
-
-    int len = 0;
-    char *str = arr;
-
-    while (str != NULL) {
-        len++;
-        str++;
+ 
+    /*
+    Verify if the string is a valid question:
+     if so, accept it -> copy it in filtered lines 
+     (else, do nothing)
+     */
+    for (line; line < array_size; line++) {
+        char c = getc(rawlines[line]);
+        while (is_whitechar(c)) {
+            c = getc(rawlines[line]);
+        }
+        // c is now the first non-white char of line
+        
+        if (c = '-') { // valid line
+            fgets(buffer, MAX_LINE_LENGTH, rawlines[line]);
+            filtered_lines[line] = malloc((strlen(buffer) + 1) * sizeof(char));
+            if (!filtered_lines[line]) {
+                fprintf(stderr, "Error while allocating memory for line.\n");
+                return 2;
+            }
+            strcpy(filtered_lines[line], buffer);
+        }
     }
 
-    return len;
+    return filtered_lines;
 }
