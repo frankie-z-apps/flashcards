472abecf99c128ee62ebb8490971432cd1a3ceb9
Francesco Zappalà <zappala.francesco00@gmail.com>
2024-06-10 23:25:18 +0200

    add logic to load previous game-state or to start a new game

--- main.c
+++ main.c
@@ -3,36 +3,57 @@
 
 #include "create_deck.h"
 #include "shuffle_deck.h"
+#include "start_menu.h"
 
 #define MAX_INPUT_LENGTH 1024
 
+int play_game(char **deck);
+char **load_deck(char *input);
+
 int main(int argc, char const *argv[])
 {
+    int user_choice = start_menu();
     char user_input[MAX_INPUT_LENGTH] = { };
 
-    fprintf(stdout, "Please insert text filename from which to extract card texts, then press ENTER: >");
-    
-    fgets(user_input, MAX_INPUT_LENGTH, stdin);
-    user_input[strcspn(user_input, "\n")] = '\0';
+    if (user_choice == 2) {
+        printf("Well, see ya!\n");
+        return 0;
+    } else if (user_choice == 1) {
+        fprintf(stdout, "Please insert your deck filename, then press ENTER: >");
+        fgets(user_input, MAX_INPUT_LENGTH, stdin);
+        user_input[strcspn(user_input, "\n")] = '\0';
+        char **loaded_deck = load_deck(user_input);
+        play_game(loaded_deck);
+    } else if (user_choice == 0) {
+        while (getchar() != '\n');
+        fprintf(stdout, "Please insert text filename from which to extract card texts, then press ENTER: >");
+        fgets(user_input, MAX_INPUT_LENGTH, stdin);
+        user_input[strcspn(user_input, "\n")] = '\0';
+        char **deck = create_deck(user_input);
+        
+        // Now I want to shuffle the deck
+        char **shuffled_deck = shuffle_deck(deck);
 
-    char **deck = create_deck(user_input);
+        ///* test for shuffled deck
+        while (*shuffled_deck) {
+            fprintf(stdout, "%s\n", *shuffled_deck);
+            shuffled_deck++;
+        }
+        //*/
 
-    /* test for deck
-    while (*deck) {
-        fprintf(stdout, "%s\n", *deck);
-        deck++;
+        play_game(shuffled_deck);
     }
-    */
-    
-    // Now I want to shuffle the deck
-    char **shuffled_deck = shuffle_deck(deck);
-
-    ///* test for shuffled deck
-    while (*shuffled_deck) {
-        fprintf(stdout, "%s\n", *shuffled_deck);
-        shuffled_deck++;
-    }
-    //*/
-
     return 0;
 }
+
+
+int play_game(char **deck) {
+    printf("Playing game with deck!\n");
+    return 0;
+}
+
+
+char **load_deck(char *input) {
+    printf("Deck is now loaded!\n");
+    return NULL;
+} 
